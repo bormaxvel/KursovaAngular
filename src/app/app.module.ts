125d6b0e78597296ff4aac57324a81160dab1fe4
Borysman <bormaxv@gmail.com>
2021-11-13 23:15:40 +0200

    Co-authored-by: OleksandrHorbun <OleksandrHorbun@users.noreply.github.com> Co-authored-by: TheValik <TheValik@users.noreply.github.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { LabsComponent } from './labs/labs.component';
+import { RouterModule } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+
+const routes = [
+  {path: '', component: MainComponent},
+  {path: 'labs', component: LabsComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    LabsComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
